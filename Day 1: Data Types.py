c4937c7b2631814ff556446e206b5d386b56bdd3
VishalSahu <52187641+VishalSahu@users.noreply.github.com>
2021-01-28 17:38:57 +0530

    Create Day 1: Data Types.py

---
+++ Day 1: Data Types.py
@@ -0,0 +1,16 @@
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+i1=int(input())
+d1=float(input())
+s1=input()
+print(i+i1)
+print(d+d1)
+print(s+s1)
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
