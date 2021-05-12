ceaef4a779c4f272b5abf9ae9c3881e2c7e92a95
VishalSahu <52187641+VishalSahu@users.noreply.github.com>
2021-05-12 08:33:31 +0530

    Create Day 13: Abstract Classes.py

---
+++ Day 13: Abstract Classes.py
@@ -0,0 +1,12 @@
+
+class MyBook(Book):
+    def __init__(self,title,author,price):
+        Book.__init__(self,title,author)
+        self.price = price
+    def display(self):
+        print("Title: "+ self.title)
+        print("Author: "+ self.author)
+        print("Price: "+ str(self.price))
+#Write MyBook class
+#Write MyBook class
+
