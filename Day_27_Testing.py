e5433dc5c5dbdb4b02cef30799bd5d6cb8cf3dac
VishalSahu <vishalsahu.live@gmail.com>
2021-05-13 18:30:01 +0530

    Day 27,28,29 added

---
+++ Day_27_Testing.py
@@ -0,0 +1,32 @@
+
+class TestDataEmptyArray(object):
+    
+    @staticmethod
+    def get_array():
+        return []
+        # complete this function
+
+class TestDataUniqueValues(object):
+
+    @staticmethod
+    def get_array():
+        return [3,1,2]
+        # complete this function
+
+    @staticmethod
+    def get_expected_result():
+        return 1
+        # complete this function
+
+class TestDataExactlyTwoDifferentMinimums(object):
+
+    @staticmethod
+    def get_array():
+        return [3,1,1]
+        # complete this function
+
+    @staticmethod
+    def get_expected_result():
+        return 1
+        # complete this function
+
